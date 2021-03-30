7d5d74edb4d0c7315f82c7b076c60ce90276daa6
Leandro Vinicius da Silva Vargas <34316242+Leandrovargas0@users.noreply.github.com>
2021-03-30 19:06:25 -0400

    layout

--- pages/CadastrarPet.php
+++ pages/CadastrarPet.php
@@ -11,51 +11,61 @@
    <body>
     <div><?php include('header.php');?></div>
     <form>
-        <fieldset class="fieldset-center">
+        <fieldset class="fieldset-center lheigth">
             <legend>Cadastro de Pets</legend>
+            <div style="width: 45%; display: inline-block;">
+                <div>
+                    <label for="txtNomePet">Nome do Seu Pet: </label>
+                    <input id="txtNomePet" name="NomePet" type="text" class="normalizadorlayout">
+                </div>
             
-            <div>
-                <label for="txtNomePet">Nome do Pet</label><br>
-                <input id="txtNomePet" name="NomePet" type="text">
+                <div>
+                    <label for="ddlTipoPet">Espécie:</label>
+                    <select class="normalizadorlayout" id="ddlTipoPet" name="TipoPet">
+                        <option value="">Selecione</option>
+                        <option value="1">Cachorro</option>
+                        <option value="2">Gato</option>
+                        <option value="3">Papagaio</option>
+                        <option value="4">Periquito</option>
+                        <option value="5">Camundongo</option>
+                      </select>
+                </div>  
+                <div>
+                    <label for="txtSexoPet">Sexo: </label>
+                    <input id="txtSexoPet1" name="txtSexoPet" type="radio" value="M">
+                    <label for="txtSexoPet1">Macho</label>
+                    <input id="txtSexoPet2" name="txtSexoPet" type="radio" value="F">
+                    <label for="txtSexoPet2">Fêmea</label>            
+                </div>
+                <div>
+                    <label for="txtIdadePet">Idade:</label>
+                    <input class="normalizadorlayout" id="txtIdadePet" name="IdadePet" type="number">
+                </div>
+                <div>
+                    <label for="DescPet">Descrição:</label><br>
+                    <textarea class="normalizadorlayout" id="DescPet" name="DescPet" rows="3" cols="33"></textarea>
+                </div>
+                <div>
+                    <label for="imgPet">Cadastre Aqui a Foto de Perfil do seu PET</label><br>
+                    <input id="imgPet" name="imgPet" type="file">
+                </div>
+                <br>
+                <div>
+                    <button class="btn_" type="submit">Cadastrar PET</button>
+                </div>
+            </div>
+            <div style="width: 45%; display: inline-block;  min-height: 300px !important" class="fieldset-center">
+                <div style="margin: 0 10px;">
+                   <h4>Exemplos de descrição:</h4>
+                   <p>1 - O meu pet tem problemas nas patas da esquerda e requer cuidado especial</p>   
+                   <hr/>
+                   <p>2 - O meu pet gosta muito de carinho</p>   
+                </div>
             </div>
 
-            <div>
-                <label for="ddlTipoPet">Tipo de Pet</label><br>
-                <select id="ddlTipoPet" name="TipoPet">
-                    <option value="">Selecione</option>
-                    <option value="1">Cachorro</option>
-                    <option value="2">Gato</option>
-                    <option value="3">Papagaio</option>
-                    <option value="4">Periquito</option>
-                    <option value="5">Camundongo</option>
-                  </select>
-            </div>  
-            <div>
-                <label for="txtSexoPet">Sexo</label><br>
-                <input id="txtSexoPet1" name="txtSexoPet" type="radio" value="M">
-                <label for="txtSexoPet1">Macho</label>
-                <input id="txtSexoPet2" name="txtSexoPet" type="radio" value="F">
-                <label for="txtSexoPet2">Fêmea</label>            
-            </div>
-            <div>
-                <label for="txtIdadePet">Idade</label><br>
-                <input id="txtIdadePet" name="IdadePet" type="number">
-            </div>
-            <div>
-                <label for="DescPet">Descricao</label><br>
-                <textarea id="DescPet" name="DescPet" rows="5" cols="33"></textarea>
-            </div>
-            <div>
-                <label for="imgPet">Cadastre Aqui a Foto de Perfil do seu PET</label><br>
-                <input id="imgPet" name="imgPet" type="file">
-            </div>
-            <br>
-            <div>
-                <button class="btn_" type="submit">Cadastrar PET</button>
-            </div>
         </fieldset>
     </form>
-    <div><?php include('footer.php');?></div>
+    <!--<div><?php include('footer.php');?></div>-->
    </body>
 </html>
 
