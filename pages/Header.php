61b4feb0618f3f1026b204b713f1db581f5e724c
Leandro Vinicius da Silva Vargas <34316242+Leandrovargas0@users.noreply.github.com>
2021-04-08 13:33:58 -0400

    Menu principal

--- pages/Header.php
+++ pages/Header.php
@@ -1,5 +1,9 @@
+<?php
+	session_start();  
+?>		
 <header class="header-master">
     <nav class="nav-master">
+		<!--
 		<div class="div-1-master" style="display: inline; float: left;">		  		
 	  			<img class="img-header-logo" src="../images/doge.jpg">
 		</div>
@@ -9,7 +13,7 @@
 	  			session_start();  
 	  			echo  $_SESSION['nome'];
 	  			?></p>
-		</div>
+		</div>-->
 
         <ul style="display: inline;">		  
 		  <li><a href="#">Publicações</a></li>
