e37c7c125e7cfd3828ebee39c4f4334fa0794e20
Gambitxt <joao0554661@gmail.com>
2021-04-14 22:24:00 -0300

    ValidaUsuarioJaCadastrado

--- pages/CadastroUsuarios.php
+++ pages/CadastroUsuarios.php
@@ -56,6 +56,10 @@
             num = num.replace(/\D/g, "")
             return num
         }
+
+        function alerta(text) {
+            alert(text);
+        }
     </script>
 </head>
 
@@ -72,59 +76,59 @@
     include('view/binarios.php');
     //$conn = new conexao;
 
-    function inserefoto(){
-    if(isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0){
-        $filename = $_FILES["photo"]["tmp_name"];  
-        $out = ImgParaBase64($filename);
-        $conn2 = new conexao;
+    function inserefoto()
+    {
+        if (isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0) {
+            $filename = $_FILES["photo"]["tmp_name"];
+            $out = ImgParaBase64($filename);
+            $conn2 = new conexao;
 
-        if (isset($_SESSION['id']))
-        {
-            $conn2->UPDATERETURN("DELETE IMAGEM_USUARIO WHERE USUARIOS_ID = " . $_SESSION['id']  );
-            $conn2->UPDATERETURN("INSERT INTO IMAGEM_USUARIO (USUARIOS_ID, DADOS , TIPO) values (". $_SESSION['id'] ." , '" . $out . "' , 'jpg' )");
-        }
-        else {
-            $max = $conn2 -> SelectReturn("SELECT MAX(USUARIOS_ID) AS ID from USUARIOS");
-            $idnovo = '';
-            if (count($max) > 1)
-            {
-                $idnovo = $max[1][0];
+            if (isset($_SESSION['id'])) {
+                $conn2->UPDATERETURN("DELETE IMAGEM_USUARIO WHERE USUARIOS_ID = " . $_SESSION['id']);
+                $conn2->UPDATERETURN("INSERT INTO IMAGEM_USUARIO (USUARIOS_ID, DADOS , TIPO) values (" . $_SESSION['id'] . " , '" . $out . "' , 'jpg' )");
+            } else {
+                $max = $conn2->SelectReturn("SELECT MAX(USUARIOS_ID) AS ID from USUARIOS");
+                $idnovo = '';
+                if (count($max) > 1) {
+                    $idnovo = $max[1][0];
+                } else {
+                    $idnovo = 1;
+                }
+                //$conn->UPDATERETURN("DELETE IMAGEM_USUARIO WHERE USUARIOS_ID = " . $idnovo  );
+                $conn2->UPDATERETURN("INSERT INTO IMAGEM_USUARIO (USUARIOS_ID, DADOS , TIPO) values (" . $idnovo . " , '" . $out . "' , 'jpg' )");
             }
-            else {
-                $idnovo = 1;
-            }
-            //$conn->UPDATERETURN("DELETE IMAGEM_USUARIO WHERE USUARIOS_ID = " . $idnovo  );
-            $conn2->UPDATERETURN("INSERT INTO IMAGEM_USUARIO (USUARIOS_ID, DADOS , TIPO) values (". $idnovo ." , '" . $out . "' , 'jpg' )");
-     
-
         }
-
     }
-}
 
     if (isset($_POST["Cadastro"])) {
-        if ($_POST["Cadastro"] == 2) {
-            $conn->UPDATERETURN(" UPDATE USUARIOS SET NOME = '" . $_POST["Nome"] . "'  WHERE USUARIOS_ID = " . $_SESSION['id']);
-            if(isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0){
-                inserefoto();
-            }
-            header("Location: CadastroUsuarios.php");
+        $qtd = $conn->SelectReturn("select count(USUARIOS_ID) as qtd from USUARIOS where email ='" . $_POST["Email"] . "' ");
+
+        if ($qtd >= 1) {
+            echo "<script> alerta('Já existe um usuário cadastrado com esse e-mail em nosso banco de dados!'); </script>";
         } else {
-            $conn->UPDATERETURN("insert into usuarios values ('" . utf8_decode($_POST["Nome"]) . "', '" . $_POST["Email"] . "', '" . $_POST["Senha"] . "', '" . $_POST["cpf"] . "', '" . $_POST["Telefone"] . "', '" . utf8_decode($_POST["Desc"]) . "', '" . $_POST["Endereço"] . "')");
-            inserefoto();
-            $maxid = $conn -> SelectReturn("SELECT MAX(USUARIOS_ID) AS ID from USUARIOS");
-            $_SESSION['id'] = $maxid[1][0];
-            header("Location: CadastroUsuarios.php");
+            if ($_POST["Cadastro"] == 2) {
+                $conn->UPDATERETURN(" UPDATE USUARIOS SET NOME = '" . $_POST["Nome"] . "'  WHERE USUARIOS_ID = " . $_SESSION['id']);
+                if (isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0) {
+                    inserefoto();
+                }
+                header("Location: CadastroUsuarios.php");
+            } else {
+                $conn->UPDATERETURN("insert into usuarios values ('" . utf8_decode($_POST["Nome"]) . "', '" . $_POST["Email"] . "', '" . $_POST["Senha"] . "', '" . $_POST["cpf"] . "', '" . $_POST["Telefone"] . "', '" . utf8_decode($_POST["Desc"]) . "', '" . $_POST["Endereço"] . "')");
+                inserefoto();
+                $maxid = $conn->SelectReturn("SELECT MAX(USUARIOS_ID) AS ID from USUARIOS");
+                $_SESSION['id'] = $maxid[1][0];
+                header("Location: CadastroUsuarios.php");
+            }
         }
-    } 
-    
+    }
+
     ?>
 
-    <?php  
-    if (isset($_SESSION['id'])){
-        $userperfil = $conn -> SelectReturn("SELECT * FROM IMAGEM_USUARIO WHERE USUARIOS_ID = " . $_SESSION['id'] );   
+    <?php
+    if (isset($_SESSION['id'])) {
+        $userperfil = $conn->SelectReturn("SELECT * FROM IMAGEM_USUARIO WHERE USUARIOS_ID = " . $_SESSION['id']);
 
-        if (count($userperfil) > 1){
+        if (count($userperfil) > 1) {
             echo '
                 <div style="width: 100%;  height: 70px;padding: 0;
                             display: -webkit-box;display: -webkit-flex;
@@ -145,14 +149,20 @@
             <legend>Cadastro de Usuários</legend>
             <div>
                 <label for="txtNome">Nome</label><br>
-                <input id="txtNome" name="Nome" type="text" class="normalizadorlayout width95porcem" 
-                value="<?php if (isset($_SESSION['nome'])){echo $_SESSION['nome'];}else{echo '';}?>">
+                <input id="txtNome" name="Nome" type="text" class="normalizadorlayout width95porcem" value="<?php if (isset($_SESSION['nome'])) {
+                                                                                                                echo $_SESSION['nome'];
+                                                                                                            } else {
+                                                                                                                echo '';
+                                                                                                            } ?>">
             </div>
 
             <div>
                 <label for="txtEmail">E-mail</label><br>
-                <input id="txtEmail" name="Email" type="text" class="normalizadorlayout width95porcem"
-                value="<?php if (isset($_SESSION['email'])){echo $_SESSION['email'];}else{echo '';}?>">
+                <input id="txtEmail" name="Email" type="text" class="normalizadorlayout width95porcem" value="<?php if (isset($_SESSION['email'])) {
+                                                                                                                    echo $_SESSION['email'];
+                                                                                                                } else {
+                                                                                                                    echo '';
+                                                                                                                } ?>">
             </div>
 
             <div style="display: inline-block;">
@@ -185,13 +195,11 @@
             </div>
 
             <div>
-                <button name="Cadastro" class="btn_" type="submit" 
-                value="<?php if ($_SESSION['id'] == "") 
-                {
-                    echo 1;
-                } else {
-                    echo 2;
-                } ?>">Cadastrar</button>
+                <button name="Cadastro" class="btn_" type="submit" value="<?php if ($_SESSION['id'] == "") {
+                                                                                echo 1;
+                                                                            } else {
+                                                                                echo 2;
+                                                                            } ?>">Cadastrar</button>
             </div>
         </fieldset>
     </form>
