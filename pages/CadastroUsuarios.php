7298918b4c6ca95c7540bb1734da04915a2593fe
Leandro Vinicius da Silva Vargas <34316242+Leandrovargas0@users.noreply.github.com>
2021-04-16 16:53:46 -0400

    VERSÃO 2 COMMIT 2
    MELHORIA NA TELA DO PERFIL

--- pages/CadastroUsuarios.php
+++ pages/CadastroUsuarios.php
@@ -67,14 +67,12 @@
     <div><?php include('header.php'); ?></div>
     <?php
 
-    //if(!isset($_SESSION['id'])){
-    //    header("Location: ../index.php");
-    //}
+    if (isset($_SESSION['id'])) {
+        $dadosuser = $conn->SelectReturn("SELECT * FROM USUARIOS WHERE USUARIOS_ID = " . $_SESSION['id']);
+    }
 
-    //$var = $_SESSION['nome'];
-    //include('view/conn.php');
     include('view/binarios.php');
-    //$conn = new conexao;
+
 
     function inserefoto()
     {
@@ -103,21 +101,59 @@
     if (isset($_POST["Cadastro"])) {
         $qtd = $conn->SelectReturn("SELECT * from USUARIOS where email ='" . $_POST["Email"] . "' ");
 
-        if ( (count($qtd)>1) && $_POST["Cadastro"] == '1'  ) {
+        if ((count($qtd) > 1) && $_POST["Cadastro"] == '1') {
             echo "<script> alerta('Já existe um usuário cadastrado com esse e-mail em nosso banco de dados!'); </script>";
         } else {
-            if ($_POST["Cadastro"] == 2) {
-                $conn->UPDATERETURN(" UPDATE USUARIOS SET NOME = '" . $_POST["Nome"] . "'  WHERE USUARIOS_ID = " . $_SESSION['id']);
-                if (isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0) {
-                    inserefoto();
+            if ($_POST["Cadastro"] == 2) 
+            {
+                if (!($_POST["Nome"] == ''
+                || $_POST["Email"] == ''
+                || $_POST["Senha"] == ''
+                || $_POST["cpf"] == ''
+                || $_POST["Telefone"] == ''
+                || $_POST["Desc"] == ''
+                || $_POST["Endereço"] == ''
+                ))
+                {
+                    $conn->UPDATERETURN(" UPDATE USUARIOS SET NOME = '" . $_POST["Nome"] . "' ,
+                    EMAIL = '".$_POST["Email"]."',
+                    USR_HASH = '".$_POST["Senha"]."',
+                    CPF = '".$_POST["cpf"]."',
+                    TELEFONE ='".$_POST["Telefone"]."',
+                    ENDERECO = '".$_POST["Endereço"]."',
+                    DESCRICAO = '".$_POST["Desc"]."'  
+                     WHERE USUARIOS_ID = " . $_SESSION['id']);
+                    if (isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0) {
+                        inserefoto();
+                    }
+                    header("Location: CadastroUsuarios.php");
                 }
-                header("Location: CadastroUsuarios.php");
-            } else {
-                $conn->UPDATERETURN("insert into usuarios values ('" . utf8_decode($_POST["Nome"]) . "', '" . $_POST["Email"] . "', '" . $_POST["Senha"] . "', '" . $_POST["cpf"] . "', '" . $_POST["Telefone"] . "', '" . utf8_decode($_POST["Desc"]) . "', '" . $_POST["Endereço"] . "')");
-                inserefoto();
-                $maxid = $conn->SelectReturn("SELECT MAX(USUARIOS_ID) AS ID from USUARIOS");
-                $_SESSION['id'] = $maxid[1][0];
-                header("Location: CadastroUsuarios.php");
+                else{
+                    echo "<script> alert('Preencha todos os campos!'); </script>";
+                }
+            } 
+            else 
+            {
+                if (!($_POST["Nome"] == ''
+                || $_POST["Email"] == ''
+                || $_POST["Senha"] == ''
+                || $_POST["cpf"] == ''
+                || $_POST["Telefone"] == ''
+                || $_POST["Desc"] == ''
+                || $_POST["Endereço"] == ''
+                || isset($_FILES["photo"])
+                ))
+                {
+                    $conn->UPDATERETURN("INSERT INTO USUARIOS values ('" . $_POST["Nome"] . "', '" . $_POST["Email"] . "', '" . $_POST["Senha"] . "', '" . $_POST["cpf"] . "', '" . $_POST["Telefone"] . "', '" . $_POST["Desc"] . "', '" . $_POST["Endereço"] . "')");
+                    inserefoto();
+                    $maxid = $conn->SelectReturn("SELECT MAX(USUARIOS_ID) AS ID from USUARIOS");
+                    $_SESSION['id'] = $maxid[1][0];
+                    header("Location: CadastroUsuarios.php");
+                }
+                else{
+                    echo "<script> alert('Preencha todos os campos e insira uma foto!'); </script>";
+                }
+                
             }
         }
     }
@@ -149,45 +185,71 @@
             <legend>Cadastro de Usuários</legend>
             <div>
                 <label for="txtNome">Nome</label><br>
-                <input id="txtNome" name="Nome" type="text" class="normalizadorlayout width95porcem" value="<?php if (isset($_SESSION['nome'])) {
-                                                                                                                echo $_SESSION['nome'];
-                                                                                                            } else {
-                                                                                                                echo '';
-                                                                                                            } ?>">
+                <input id="txtNome" name="Nome" type="text" class="normalizadorlayout width95porcem" 
+                value="<?php                  
+                if (isset($_SESSION['id'])) {
+                    echo $dadosuser[1][1];
+                } ?>">
             </div>
 
             <div>
                 <label for="txtEmail">E-mail</label><br>
-                <input id="txtEmail" name="Email" type="text" class="normalizadorlayout width95porcem" value="<?php if (isset($_SESSION['email'])) {
-                                                                                                                    echo $_SESSION['email'];
-                                                                                                                } else {
-                                                                                                                    echo '';
-                                                                                                                } ?>">
+                <input id="txtEmail" name="Email" type="text" class="normalizadorlayout width95porcem" 
+                value="<?php                  
+                if (isset($_SESSION['id'])) {
+                    echo $dadosuser[1][2];
+                } ?>">
             </div>
 
             <div style="display: inline-block;">
                 <label for="txtCPF">CPF</label><br>
-                <input id="txtCPF" type="text" name="cpf" onkeydown="javascript: Masc( this, CPF );" class="normalizadorlayout">
+                <input id="txtCPF" type="text" name="cpf" onkeydown="javascript: Masc( this, CPF );" 
+                class="normalizadorlayout"
+                value="<?php                  
+                if (isset($_SESSION['id'])) {
+                    echo $dadosuser[1][4];
+                } ?>"
+                >
             </div>
 
             <div style="display: inline-block;">
                 <label for="txtTelefone">Telefone</label><br>
-                <input id="txtTelefone" name="Telefone" type="txt" onkeydown="javascript: Masc( this, Tel );" class="normalizadorlayout">
+                <input id="txtTelefone" name="Telefone" type="txt" onkeydown="javascript: Masc( this, Tel );" 
+                class="normalizadorlayout"
+                value="<?php                  
+                if (isset($_SESSION['id'])) {
+                    echo $dadosuser[1][5];
+                } ?>"
+                >
             </div>
 
             <div style="display: inline-block;">
                 <label for="txtSenha">Senha</label><br>
-                <input id="txtSenha" name="Senha" type="password" class="normalizadorlayout">
+                <input id="txtSenha" name="Senha" type="password" 
+                class="normalizadorlayout"
+                value="<?php                  
+                if (isset($_SESSION['id'])) {
+                    echo $dadosuser[1][3];
+                } ?>"
+                >
             </div>
 
             <div>
                 <label for="txtEnd">Endereço</label><br>
-                <input id="txtEnd" name="Endereço" type="txt" class="normalizadorlayout width95porcem">
+                <input id="txtEnd" name="Endereço" type="txt" class="normalizadorlayout width95porcem"
+                value="<?php                  
+                if (isset($_SESSION['id'])) {
+                    echo $dadosuser[1][6];
+                } ?>"
+                >
             </div>
 
             <div>
                 <label for="DescPet">Descrição</label><br>
-                <textarea class="normalizadorlayout width95porcem" id="Desc" name="Desc" rows="3" cols="33"></textarea>
+                <textarea class="normalizadorlayout width95porcem" id="Desc" name="Desc" rows="3" cols="33"><?php                  
+                if (isset($_SESSION['id'])) {
+                    echo $dadosuser[1][7];
+                } ?></textarea>
             </div>
             <div>
                 <label for="imgPet">Insira sua foto</label><br>
